a0a9b4bad1564ecab38968ee49a57f7460817cd5
Andrei Kashchei <375296218881g@gmail.com>
2022-06-29 22:22:09 +0200

    Please rewiew realisation and  if all goes well I’ll make adaptive layout

---
+++ src/js/modules/modalWindowAmount.js
@@ -0,0 +1,68 @@
+const spanAdult = Array.from(document.getElementsByClassName('adults-count'));
+const spanChildren = Array.from(document.getElementsByClassName('children-count'));
+const spanRoom = Array.from(document.getElementsByClassName('rooms-count'));
+const amount = document.getElementById('amount');
+const modalWindowAmount = document.getElementById('modalAmount');
+const btnAmount = modalWindowAmount.querySelectorAll('svg');
+const liBabyAge = document.getElementById('li-baby-age');
+
+let childrenCount = 0;
+let roomsCount = 0;
+let adultsCount = 0;
+
+amount.addEventListener('click', () => {
+  modalWindowAmount.classList.toggle('modal--visible');
+});
+
+const addSelect = () => {
+  const cloneSelect = document.getElementById('baby-age').cloneNode(true);
+
+  cloneSelect.dataset.index = childrenCount;
+  liBabyAge.insertAdjacentElement('beforeend', cloneSelect);
+};
+
+const removeSelect = () => {
+  const listSelects = Array.from(liBabyAge.querySelectorAll('select'));
+
+  listSelects.forEach((select) => {
+    if (select.dataset.index === `${childrenCount}`) select.remove();
+  });
+};
+
+btnAmount.forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    if (event.currentTarget.dataset.name === 'adult-minus' && adultsCount > 0) {
+      adultsCount -= 1;
+      spanAdult.forEach((el) => (el.innerText = adultsCount));
+    }
+
+    if (event.currentTarget.dataset.name === 'adult-plus' && adultsCount < 30) {
+      adultsCount += 1;
+      spanAdult.forEach((el) => (el.innerText = adultsCount));
+    }
+
+    if (event.currentTarget.dataset.name === 'children-minus' && childrenCount > 0) {
+      childrenCount -= 1;
+
+      childrenCount > 0 ? removeSelect() : (liBabyAge.style.display = 'none');
+
+      spanChildren.forEach((el) => (el.innerText = childrenCount));
+    }
+
+    if (event.currentTarget.dataset.name === 'children-plus' && childrenCount < 10) {
+      childrenCount > 0 ? addSelect() : (liBabyAge.style.display = 'flex');
+
+      childrenCount += 1;
+      spanChildren.forEach((el) => (el.innerText = childrenCount));
+    }
+
+    if (event.currentTarget.dataset.name === 'room-minus' && roomsCount > 0) {
+      roomsCount -= 1;
+      spanRoom.forEach((el) => (el.innerText = roomsCount));
+    }
+    if (event.currentTarget.dataset.name === 'room-plus' && roomsCount < 30) {
+      roomsCount += 1;
+      spanRoom.forEach((el) => (el.innerText = roomsCount));
+    }
+  });
+});
